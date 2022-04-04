aa050bc5e6980f238d4515bceb87ffbd9a812391
Justin Callies <jcallies@gmail.com>
2022-04-04 10:16:50 -0500

    add dinosaur file

---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self):
+        self.dinosaur_name = "Rex"
+        self.attack_power = 20
+        self.dinosaur_health = 100
+
+    def dino_attack(self):
+        pass
