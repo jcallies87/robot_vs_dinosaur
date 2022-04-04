d7d1b6666f53772d16076f1adcfc89e7725eb764
Justin Callies <jcallies@gmail.com>
2022-04-04 11:38:21 -0500

    added dino and robot attacks

--- dinosaur.py
+++ dinosaur.py
@@ -1,8 +1,10 @@
+from robot import Robot
 class Dinosaur:
     def __init__(self):
         self.dinosaur_name = "Rex"
         self.attack_power = 20
         self.dinosaur_health = 100
 
-    def dino_attack(self):
-        pass
+    def dino_attack(self , robot):
+        robot = Robot
+        robot.robot_health -= self.attack_power
--- robot.py
+++ robot.py
@@ -1,8 +1,11 @@
 from weapon import Weapon
+from dinosaur import Dinosaur
 class Robot:
     def __init__(self):
         self.robot_name = 'Alloy_Cat'
         self.robot_health= 100
+        self.active_weapon = Weapon
 
-    def robot_attack(self):
-        pass
+    def robot_attack(self, dinosaur):
+        dinosaur = Dinosaur
+        dinosaur.dinosaur_health -= (self.active_weapon.weapon_power)
